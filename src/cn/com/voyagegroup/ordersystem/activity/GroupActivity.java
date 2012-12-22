b49ccd43bb5b62be5d3f0e077a05856061a9240a
ljxwjj <ljxwjj@163.com>
2012-12-22 14:54:02 +0800

    fix

--- src/cn/com/voyagegroup/ordersystem/activity/GroupActivity.java
+++ src/cn/com/voyagegroup/ordersystem/activity/GroupActivity.java
@@ -1,16 +1,23 @@
 package cn.com.voyagegroup.ordersystem.activity;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import cn.com.voyagegroup.ordersystem.R;
 
 public class GroupActivity extends Activity {
 
 	private ListView mListView;
+	private MyAdapter mAdapter;
+	private ArrayList mData;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +27,40 @@ public class GroupActivity extends Activity {
 
 		mListView = (ListView) findViewById(R.id.list_view);
 		mListView.addFooterView(createFooterView());
+		
+		mData = new ArrayList();
+		mAdapter = new MyAdapter();
+		mListView.setAdapter(mAdapter);
 	}
 
 	private View createFooterView() {
 		LayoutInflater mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View view = mInflater.inflate(R.layout.footer_layout, null);
 		return view;
+	}
+	
+	public void createGroup(View view) {
+		Intent intent = new Intent(this, RegisterGroupActivity.class);
+		startActivity(intent);
+	}
+	
+	class MyAdapter extends BaseAdapter {
+
+		public int getCount() {
+			return 0;
+		}
+
+		public Object getItem(int arg0) {
+			return null;
+		}
+
+		public long getItemId(int arg0) {
+			return 0;
+		}
+
+		public View getView(int arg0, View arg1, ViewGroup arg2) {
+			return null;
+		}
+		
 	}
 }
