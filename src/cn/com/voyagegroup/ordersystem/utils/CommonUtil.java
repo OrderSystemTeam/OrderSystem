c3dfb14efb6c9b66aba6366f450eaa11bcffca9e
ljxwjj <ljxwjj@163.com>
2012-12-22 12:25:55 +0800

    aaa

--- src/cn/com/voyagegroup/ordersystem/utils/CommonUtil.java
+++ src/cn/com/voyagegroup/ordersystem/utils/CommonUtil.java
@@ -2,8 +2,20 @@ package cn.com.voyagegroup.ordersystem.utils;
 
 import java.util.Map;
 
+import com.google.gson.Gson;
+
 public class CommonUtil {
-    public static String mapToJson(Map dataMap) {
-        return null;
-    }
+
+	public static Gson gson = new Gson();
+
+	public static String mapToJson(Map dataMap) {
+		return null;
+	}
+
+	/**
+	 * parse object to json string.
+	 */
+	public static String parseToJson(Object object) {
+		return gson.toJson(object);
+	}
 }
